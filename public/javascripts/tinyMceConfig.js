16db0e10cd8c3c3e71359c5d7038960df78055b5
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2015-12-12 09:19:20 +0530

    Removed title of the journal.

--- public/javascripts/tinyMceConfig.js
+++ public/javascripts/tinyMceConfig.js
@@ -12,13 +12,6 @@ $(function(){
 	    body_class: 'tinymceCustomBody',
 	    content_css: '/stylesheets/style.css',
 	    statusbar: false,
-	    setup: function(ed){
-            ed.on("init",
-                function(ed) {
-                    tinyMCE.get('journal').setContent("<h1 id='mcetitle'> Title ...</h1>");
-                }
-            );
-        },
 	    toolbar: "insertfile undo redo | fontselect styleselect fontsizeselect | bold italic underline forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | submitButton"
 	});
 });

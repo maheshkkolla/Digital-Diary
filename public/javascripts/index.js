49518629f7d13137113a42b4ef6e4e777f48a98f
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2016-03-12 16:10:39 +0530

    Using journal model

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -3,24 +3,42 @@ var Journal = function(id, dateTime, content) {
 	this.id = id;
 	this.dateTime = dateTime;
 	this.content = content;
+
+
+	Object.defineProperty(this, 'updateOptions', { value: {
+		url: '/journals/ID',
+		type: 'PUT'
+	}});
+
+	Object.defineProperty(this, 'createOptions', { value: {
+		url: '/journals',
+		type: 'POST'
+	}});
 };
 
 Journal.prototype = {
 	save: function() {
-		if(isNullOrUndefined(this.id)) {
-			this.createNewJournal();
+		if(u.isNullOrUndefined(this.id)) {
+			return this.createNewJournal();
 		} else {
-			this.updateTheJournal();
+			return this.updateTheJournal();
 		}
 	},
 
 	createNewJournal: function () {
-
+		return new Ajax(this.createOptions).setData(this.toJson()).call();
 	},
 
-
 	updateTheJournal: function () {
+		return new Ajax(this.updateOptions).setData(this.toJson()).setUrlParams({ID: this.id}).call();
+	},
 
+	toJson: function() {
+		return {
+			id: this.id,
+			dateTime: this.dateTime,
+			content: this.content
+		};
 	}
 };
 
@@ -32,22 +50,18 @@ $(function() {
 
 	$('#saveJournal').on('click', function() {
 		var notification = new Notification({message: 'Creating Journal ...', autoClose: false, close: false}).notify();
-		var journal = $("#journal").html();
+		var journalContent = $("#journal").html();
 		var dateTime = $("#dateTime").val();
-		$.ajax({
-			url: '/journals',
-			type: 'POST',
-			data: {
-				'dateTime': dateTime,
-				'journal': journal
-			}
-		}).done(function (status) {
-			notification.close();
-			if(status == 'OK') {
-				new Notification({message: 'Journal created.', type: 'success'}).notify();
-			}
-			else creationFailed();
-		}).fail(creationFailed);
+		var journal = new Journal(null, dateTime, journalContent);
+
+		journal.save()
+			.done(function (status) {
+				notification.close();
+				if(status == 'OK') {
+					new Notification({message: 'Journal created.', type: 'success'}).notify();
+				}
+				else creationFailed();
+			}).fail(creationFailed);
 	});
 
 	var creationFailed = function() {

2675126f459336bcf65eeeb8cae761113b709c44
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2016-03-12 18:45:44 +0530

    Removed commented lines

--- public/javascripts/dateTimePickerConfig.js
+++ public/javascripts/dateTimePickerConfig.js
@@ -1,16 +1,3 @@
-//$(function() {
-//    $('#datetimepicker1').datetimepicker({
-//        maxDate: new Date(),
-//        defaultDate: new Date(),
-//        sideBySide: true
-//    }).bind('dp.change', function (e) {
-//    	var date = new Date(e.date);
-//    	$('#dateTime').val(date.toString()).trigger('change');
-//    });
-//    $('#dateTime').val(new Date().toString());
-//
-//
-//});
 var DateTimePicker = function(date) {
     this.element = $('#datetimepicker1').datetimepicker({
         maxDate: new Date(),

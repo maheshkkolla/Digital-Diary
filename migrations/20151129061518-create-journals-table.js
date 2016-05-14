f11076957545419251b9ecd2d17bf258eba6d1a0
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2016-05-14 13:29:53 +0530

    Upgrade db-migrate t0 0.10.1 beta because foreign key was not creating in psql

--- migrations/20151129061518-create-journals-table.js
+++ migrations/20151129061518-create-journals-table.js
@@ -4,17 +4,7 @@ var type = dbm.dataType;
 exports.up = function(db, callback) {
   db.createTable('journals', {
 		id: {type: 'int', primaryKey: true, autoIncrement: true},
-		user_id: {type:'int', notNull: true, 
-			foreignKey: {
-				name: 'journal_user_id_fk',
-				table: 'users',
-				rules: {
-            		onDelete: 'CASCADE',
-            		onUpdate: 'RESTRICT'
-          		},
-          		mapping: 'id'
-			}
-		},
+		user_id: {type:'int', notNull: true},
 		file_path: {type:'text', notNull: true},
 		date_time: {type: 'timestamp with time zone', notNull: true},
 		create_at: {type: 'timestamp with time zone'}

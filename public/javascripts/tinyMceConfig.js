42c8cf8309e6d81a3a1d9bd2a8b11a727ca5bdf3
Mahesh Kumar Kolla <mahesh.mk.245@gmail.com>
2016-06-01 16:35:35 +0530

    Fix positioning toolbar of tinymce

--- public/javascripts/tinyMceConfig.js
+++ public/javascripts/tinyMceConfig.js
@@ -18,17 +18,23 @@ $(function(){
 			});
 
 			editor.on('focus', function() {
-				firstTimeFocus && setTimeout(function() {
-					var toolbar = $("#mceu_19");
-					$(".myToolbar").append(toolbar);
-					toolbar.css('top', '0px');
-					toolbar.css('left', '0px');
-				},100);
-
+				firstTimeFocus && positionToolBar();
 				firstTimeFocus = false;
 			});
 		},
 		toolbar: "insertfile undo redo | fontselect styleselect fontsizeselect | bold italic underline forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link "
 	});
 });
+
+var positionToolBar = function() {
+	var myToolbar = $(".myToolbar");
+	var toolbar = $("#mceu_19");
+	myToolbar.append(toolbar);
+	toolbar.css('top', '0px');
+	toolbar.css('left', '0px');
+
+	if(myToolbar.find("#mce_19").length == 0) {
+		setTimeout(positionToolBar, 100);
+	}
+};
 
